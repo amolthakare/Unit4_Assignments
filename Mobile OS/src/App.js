ac8f2228653423fc06dada663ef3f38efd8f5384
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2022-05-22 23:18:20 +0530

    Add files via upload

---
+++ Mobile OS/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Mobile_OS from './components/Mobile';
+
+function App() {
+  return (
+    <div className="App">
+      <Mobile_OS></Mobile_OS>
+    </div>
+  );
+}
+
+export default App;
---
+++ Mobile OS/src/components/Mobile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Mobile_OS = () => {
+
+    return (
+        <div>
+            <h1>MOBILE OPERATING SYSTEM</h1>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windows Phone</li>
+
+            <h1>MOBILE MANUFACTURING</h1>
+            <li id="sq">Samsung</li>
+            <li id="sq">HTC</li>
+            <li>Micromax</li>
+            <li id="disc">Apple</li>
+        </div>
+    )
+}
+
+export default Mobile_OS;
